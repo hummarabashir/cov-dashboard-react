08d4da10d474eb8e3ea679a14c40d8065614b127
Hummara Bashir <hummarabashir@gmail.com>
2023-06-14 11:34:39 -0400

    commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+function App() {
+  return (
+    <div className="App">
+      <h1>COVID 19 Dashboard </h1>
+      <div className="dashboard-container">
+        <div className="dashboard-menu"></div>
+        <div className="dashboard-timeseries"></div>
+        <div className="dashboard-summary"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
